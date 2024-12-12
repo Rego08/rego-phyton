eed1296e8252fda3f9525161a7a8ec0344abe585
Rego08 <regomark866@gmail.com>
2024-12-12 09:58:41 +0200

    Create #Peo eelarve.py

---
+++ #Peo eelarve.py
@@ -0,0 +1,14 @@
+#Peo eelarve
+
+
+
+kylalistearv=int(input("Mitu inimest on kutsutud?"))
+lubanud=int(input("Mitu inimest on lubanud tulla?"))
+def eelarve(k):
+    eelarve=k*10+55
+    return eelarve
+
+
+print(eelarve(kylalistearv))
+print(eelarve(lubanud))
+
